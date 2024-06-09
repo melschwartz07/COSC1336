701433cd06cd1129e1f058f151e32d374f922ac0
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2024-06-09 16:20:18 -0500

    assignment 1

---
+++ Assignment2.cpp
@@ -0,0 +1,16 @@
+// Melanie Schwartz
+// Assignment 2
+// 6/7/2024
+
+// Preprocessor Directive
+#include <iostream>
+
+int main() {
+    // namespace, cout, newline
+    std::cout << "\nMelanie Schwartz\n"
+              << "42 Wallaby Way, Sydney, Australia, 45815\n"
+              << "210-867-5309\n"
+              << "Computer Science\n";
+    return 0;
+}
+
