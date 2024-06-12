02171472b6907af659d084307f6e5d9b2e22c539
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2024-06-11 19:02:14 -0500

    assignment 3

---
+++ Assignment3.cpp
@@ -0,0 +1,30 @@
+// Melanie Schwartz
+// Assignment # 3
+// 6/13/2024
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // define variables
+    double gallons;
+    double miles;
+
+    // Prompt user's input
+    cout << "Enter the number of gallons the car can hold: ";
+    cin >> gallons;
+    cout << "Enter the number of miles the car can be driven on a full tank: ";
+    cin >> miles;
+
+    // perform MPG calculation
+    if (gallons != 0) {
+        double milesPerGallon = miles / gallons;
+
+        // Result
+        cout << "The car's gas MPG is " << milesPerGallon << "." << endl;
+    } else {
+        cout << "The number of gallons cannot be zero." << endl;
+    }
+
+    return 0;
+}
