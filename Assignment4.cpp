426f18456605e259436399ef0b2c24cee6fd5481
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2024-06-11 19:06:17 -0500

    assignment 4

---
+++ Assignment4.cpp
@@ -0,0 +1,33 @@
+// Melanie Schwartz
+// Assignement 4
+// 6/15/2024
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Define variables
+    double score1, score2, score3, score4, score5;
+
+    // Prompt user for input
+    cout << "Enter the first test score: ";
+    cin >> score1;
+    cout << "Enter the second test score: ";
+    cin >> score2;
+    cout << "Enter the third test score: ";
+    cin >> score3;
+    cout << "Enter the fourth test score: ";
+    cin >> score4;
+    cout << "Enter the fifth test score: ";
+    cin >> score5;
+
+    // Test score average calculation
+    double average = (score1 + score2 + score3 + score4 + score5) / 5.0;
+
+    // Display the average in fixed-point notation to one decimal
+    cout << fixed << setprecision(1);
+    cout << "The average test score is " << average << endl;
+
+    return 0;
+}
